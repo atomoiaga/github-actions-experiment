da7b4edd24059c93fb826cef4d10470e304d9bc7
astomoiaga <andrei.tomoiaga@bitstone.eu>
2023-04-19 14:49:29 +0300

    Add change

--- scripts/check_branch.sh
+++ scripts/check_branch.sh
@@ -3,8 +3,8 @@
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 if [[ "$BRANCH" == "staging" ]]; then
-  git checkout develop && git merge staging && git push origin develop && git checkout staging;
-  exit 1;
+  git checkout develop && git merge staging && git push origin develop
+  exit 0;
 fi
 
-echo 'Do stuff';
+echo 'Not on staging branch.';
